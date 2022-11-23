0fc917635f5264403efc3f528c4f36db8a8765ad
mcsdevv <mail@mcs.dev>
2022-11-23 12:06:25 +0000

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
