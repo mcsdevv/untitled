11a0a0119da4b91a5f84c205655005b074ff1fb1
mcsdevv <mail@mcs.dev>
2022-11-23 21:39:50 +0000

    add tailwind

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+// * Components
+import Header from "@components/Header";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="">
+      <head />
+      <body className="overflow-y-scroll bg-gray-1100 bg-[url('/grid.svg')]">
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/Header/index.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="flex flex-row items-center justify-between w-full h-16 px-4 bg-gray-900">
+      <div className="flex justify-between">
+        <h1 className="text-xl font-bold text-gray-100">untitled</h1>
+      </div>
+      <div className="flex justify-between">
+        <h1 className="text-xl font-bold text-gray-100">untitled</h1>
+      </div>
+    </header>
+  );
+}
