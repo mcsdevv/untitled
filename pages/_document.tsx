3f7ba7745600eea0f5b5981688541f191e08df6b
mcsdevv <mail@mcs.dev>
2022-11-23 20:39:06 +0000

    add eslintrc

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+// * Libraries
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=block"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
