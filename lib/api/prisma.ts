b6b69bb7acf0f07f568a3ac4a32dde1595d95bbf
mcsdevv <mail@mcs.dev>
2022-12-03 14:41:31 +0000

    add application client

--- lib/api/prisma.ts
+++ lib/api/prisma.ts
@@ -1,5 +1,8 @@
+import { PrismaClient as ApplicationClient } from "../../prisma/clients/application";
 import { PrismaClient as MySqlClient } from "../../prisma/clients/mysql";
 import { PrismaClient as PostgresClient } from "../../prisma/clients/postgres";
+
+export const prisma = new ApplicationClient();
 
 export const neon = new PostgresClient({
   datasources: { db: { url: process.env.NEON_URL } },
