c1a0deae4b17fd0542ba63f88226af1f7979688e
mcsdevv <mail@mcs.dev>
2022-12-03 02:10:48 +0000

    remove text from landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 // * Components
-import Button from "@components/Button";
 import Graphs from "@components/Landing/graphs";
 
 export default function Home() {
@@ -7,14 +6,6 @@ export default function Home() {
     <div className="flex flex-col items-center">
       <h1 className="pt-16 pb-8 text-7xl">Compare Database Performance</h1>
       <Graphs />
-      <p className="pt-4">
-        The data above is based on a simple get user call made to each database
-        every 5 minutes.
-      </p>
-      <p className="pt-4">
-        Login to see how they perform in various other scenarios.
-      </p>
-      <Button className="mt-4" text="Login" />
     </div>
   );
 }
