c9191aa56fba247aee297ffd0a965010ff3ed585
mcsdevv <mail@mcs.dev>
2022-12-04 18:27:53 +0000

    update endpoint names

--- lib/api/prisma.ts
+++ lib/api/prisma.ts
@@ -5,13 +5,13 @@ import { PrismaClient as PostgresClient } from "../../prisma/clients/postgres";
 export const application = new ApplicationClient();
 
 export const neon = new PostgresClient({
-  datasources: { db: { url: process.env.NEON_URL } },
+  datasources: { db: { url: process.env.NEON_WARM_URL } },
 });
 
 export const supabase = new PostgresClient({
-  datasources: { db: { url: process.env.SUPABASE_URL } },
+  datasources: { db: { url: process.env.SUPABASE_WARM_URL } },
 });
 
 export const planetscale = new MySqlClient({
-  datasources: { db: { url: process.env.PLANETSCALE_URL } },
+  datasources: { db: { url: process.env.PLANETSCALE_WARM_URL } },
 });
